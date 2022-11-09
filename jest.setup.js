521e08168df0b33f12d459c61d7f29b802d75b61
deepak-wednesday <deepak@wednesday.is>
2022-11-09 19:01:10 +0530

    fix/test

--- jest.setup.js
+++ jest.setup.js
@@ -1,21 +1,9 @@
 import 'core-js';
 import AWSMock from 'aws-sdk-mock';
-import AWS from 'aws-sdk';
+
 import dotEnv from 'dotenv';
 
 dotEnv.config({ path: '.env' });
-
-const OLD_ENV = process.env;
-
-beforeEach(() => {
-	jest.clearAllMocks();
-	jest.resetAllMocks();
-	jest.resetModules();
-	process.env = {
-		...OLD_ENV,
-	};
-	AWSMock.setSDKInstance(AWS);
-});
 
 afterEach(() => {
 	AWSMock.restore();
