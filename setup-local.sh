c36f96cccc39b748675c0bbdfd6f97ce108005c3
deepak-wednesday <deepak@wednesday.is>
2023-02-24 00:37:31 +0530

    update: remove uncessary comments

--- setup-local.sh
+++ setup-local.sh
@@ -2,12 +2,10 @@
 set -x
 
 # purely for LOCAL setup.
-# creates databases and seeds it locally with whatever is available. Never run this anywhere else!!
 
 yarn
 export ENVIRONMENT_NAME=local
 
-# all application
 echo '===Creating database==='
 npx sequelize db:drop
 npx sequelize db:create 
