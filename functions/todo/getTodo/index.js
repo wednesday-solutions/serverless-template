0b43040f783135fbb90714290a12f51ec04c10a5
deepak-wednesday <deepak@wednesday.is>
2023-02-24 00:47:40 +0530

    fix: error msg for empty data

--- functions/todo/getTodo/index.js
+++ functions/todo/getTodo/index.js
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash';
 import LambdaBuilder from '@utils/lambda-builder';
 import LambdaCloser from '@utils/lambda-closer';
 import { findByUuid } from '@src/interface-adaptors/daos/todo';
@@ -9,6 +10,11 @@ const getTodo = async (event, { logger }) => {
 	const { uuid } = event.queryStringParameters;
 	try {
 		const todoList = await findByUuid({ uuid });
+		if (isEmpty(todoList)) {
+			return new LambdaCloser({
+				code: 'E4',
+			}).notFound();
+		}
 
 		return new LambdaCloser({
 			message: 'All todos for a user are',
--- utils/error-code-messages.js
+++ utils/error-code-messages.js
@@ -1,6 +1,7 @@
 const ERROR_CODE_MESSAGES = {
 	E1: 'Failed to create a todo',
 	E2: 'Failed to create and enqueue todo',
+	E4: 'Data not found',
 };
 
 export default ERROR_CODE_MESSAGES;
