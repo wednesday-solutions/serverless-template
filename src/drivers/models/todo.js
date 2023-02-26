e7eb08ea03fbe6f1c8a00bbe04890a99fd2d78ae
Abhishek Mishra <abhishek@wednesday.is>
2023-02-26 21:33:34 +0530

    chore: add association for user and todo

--- src/drivers/models/todo.js
+++ src/drivers/models/todo.js
@@ -1,7 +1,14 @@
 const { Model } = require('sequelize');
 
 const initialiseTodo = (sequelize, DataTypes) => {
-	class Todo extends Model {}
+	class Todo extends Model {
+		static associate(models) {
+			Todo.belongsTo(models.User, {
+				as: 'user',
+				foreignKey: 'uuid',
+			});
+		}
+	}
 	Todo.init(
 		{
 			uuid: DataTypes.STRING,
--- src/drivers/models/user.js
+++ src/drivers/models/user.js
@@ -1,13 +1,12 @@
 const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
 	class User extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
 		static associate(models) {
-			// define association here
+			User.hasMany(models.Todo, {
+				as: 'userTodos',
+				foreignKey: 'uuid',
+			});
 		}
 	}
 	User.init(
