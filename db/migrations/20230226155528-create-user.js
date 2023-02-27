a810726321cecf61c5180679bf1cab6ebf89fcd4
Abhishek Mishra <abhishek@wednesday.is>
2023-02-27 08:49:05 +0530

    chore: updates acc to feedback

--- db/migrations/20230226155528-create-user.js
+++ db/migrations/20230226155528-create-user.js
@@ -9,9 +9,11 @@ module.exports = {
 				type: Sequelize.INTEGER,
 			},
 			name: {
+				allowNull: false,
 				type: Sequelize.STRING,
 			},
 			uuid: {
+				allowNull: false,
 				type: Sequelize.STRING,
 			},
 			createdAt: {
