aa12e5990cdcaa47baf2458eeebd15e6d294d3f2
deepak-wednesday <deepak@wednesday.is>
2023-03-03 22:25:13 +0530

    fix: cd

--- functions/todo/create/index.js
+++ functions/todo/create/index.js
@@ -6,6 +6,7 @@ import createTodoValidator from './createTodoSchema';
 
 const createTodoHandler = async (event, { logger }) => {
 	const { title, description, uuid } = event.body;
+
 	try {
 		const newTodo = await createTodo({ uuid, title, description });
 		return new LambdaCloser({
