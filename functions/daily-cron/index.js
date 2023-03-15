8209377cb0480d92718a33fa7ed1b1e5c6128a34
deepak-wednesday <deepak@wednesday.is>
2023-03-16 00:33:19 +0530

    feat: add more tests

--- functions/daily-cron/index.js
+++ functions/daily-cron/index.js
@@ -3,7 +3,7 @@ import LambdaCloser from '@utils/lambda-closer';
 
 export const baseHandler = async (_, { logger }) => {
 	const response = 'Hey from Daily cron!';
-	logger.info(response);
+	logger?.info(response);
 	return new LambdaCloser({
 		message: response,
 	}).ok();
